b7ce4b745c9ce362dda3444bfdfa6c23079d6238
reaktivo <m@reaktivo.com>
2013-09-19 15:09:46 -0700

    Debounce instead of throttle

--- assets/js/vendor/closest-to-scroll.js
+++ assets/js/vendor/closest-to-scroll.js
@@ -25,26 +25,19 @@
 
 ;(function($, window, document, undefined) {
 
-  function throttle(fn, threshhold, scope) {
-    threshhold || (threshhold = 250);
-    var last,
-        deferTimer;
-    return function () {
-      var context = scope || this;
-
-      var now = +new Date,
-          args = arguments;
-      if (last && now < last + threshhold) {
-        // hold on to it
-        clearTimeout(deferTimer);
-        deferTimer = setTimeout(function () {
-          last = now;
-          fn.apply(context, args);
-        }, threshhold);
-      } else {
-        last = now;
-      }
-    };
+  function debounce(func, wait, immediate) {
+  	var timeout;
+  	return function() {
+  		var context = this, args = arguments;
+  		var later = function() {
+  			timeout = null;
+  			if (!immediate) func.apply(context, args);
+  		};
+  		var callNow = immediate && !timeout;
+  		clearTimeout(timeout);
+  		timeout = setTimeout(later, wait);
+  		if (callNow) func.apply(context, args);
+    }
   }
 
   $.fn.closestToScroll = function(options, fn) {
@@ -61,7 +54,7 @@
 
     var self = this, $window = $(window)
 
-    var fn = throttle(fn, options.threshhold)
+    var fn = debounce(fn, options.threshhold)
 
     $window.scroll(function() {
 
